c52604f043b3fd7efccb9fc532946d0f83b3e89a
gdstitt <greg.stitt@bigpond.com>
2018-01-30 00:18:27 +0800

    Third code commit

--- System01/src/system01/System01.java
+++ System01/src/system01/System01.java
@@ -18,11 +18,22 @@ public class System01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.getProperties().list(System.out);
         Properties p = new Properties(System.getProperties());
         
+        
+        System.out.println("\n\n\nLooking at our newly created property list.\n" +
+                "The number of properties in p is " + p.size());
         for(int count = 0; count < p.size(); count++) {
             System.out.println("Value number " + (count + 1));
         }
+        
+        
+        System.out.println("\n\n\nNOTE: even though the size() function returns zero for p," +
+                " the list() function\nwill return properties provided as defaults by " +
+                "the constructor's calling of System.getProperties()\n");
+        p.list(System.out);
     }
     
 }
