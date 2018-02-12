7d4d8feaa219029259fbe3ecfd91c62e4fd8e200
gdstitt <greg.stitt@bigpond.com>
2018-02-12 23:49:07 +0800

    Added Unix return of \n

--- System01/src/system01/System01.java
+++ System01/src/system01/System01.java
@@ -41,11 +41,18 @@ public class System01 {
         switch (ls.length()) {
             case 1: 
                 System.out.println("Unix system");
+                if (ls.contains( "\n")) {
+                    System.out.println("Line separator is \"\\n\"");
+                } else {
+                    System.out.println("The line separator has unexpected contents");
+                }
                 break;
             case 2:
                 System.out.println("Windows System");
                 if (ls.contains( "\r\n")) {
                     System.out.println("Line separator is \"\\r\\n\"");
+                } else {
+                    System.out.println("The line separator has unexpected contents");
                 }
                 break;
             default:
